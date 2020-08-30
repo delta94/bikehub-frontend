bdc4fcb6b23e256d88586643008fbd9d51d6a91f
yuta.hidaka <yuta322@gmail.com>
2020-08-30 16:30:12 +0900

    test comit

--- App.tsx
+++ App.tsx
@@ -1,21 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, FlatList, SafeAreaView } from 'react-native';
+import NewsCard from './components/NewsCard';
+import data from './data';
+import data2 from './data2';
+
+type NewsCardProps = {
+  title: string;
+  author: string;
+  imgUrl: string;
+};
 
 export default function App() {
+  const [newsData, setNews] = useState(data);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setNews(data2);
+    }, 5000);
+
+    // return () => clearTimeout(() => {});
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app@@@@@@!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.newsCardList}>
+        <FlatList
+          data={newsData}
+          renderItem={({ item }) => (
+            <NewsCard
+              title={item.title}
+              author={item.author}
+              imgUrl={item.img_url}
+            />
+          )}
+          keyExtractor={(item, index) => index.toString()}
+        />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    maxWidth: 1000,
+    margin: 'auto',
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  newsCardList: {
+    width: '100%',
   },
 });
---
+++ components/NewsCard.tsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image, ImageBackground } from 'react-native';
+import { RFPercentage } from 'react-native-responsive-fontsize';
+
+type NewsCardProps = {
+  title: string;
+  author: string;
+  imgUrl: string;
+};
+
+export default function NewsCard({ title, author, imgUrl }: NewsCardProps) {
+  return (
+    <View style={styles.itemContainer}>
+      <View style={styles.leftContainer}>
+        <Text numberOfLines={3} style={styles.text}>
+          {title}
+        </Text>
+        <Text style={styles.title}>{author}</Text>
+      </View>
+      <View style={styles.rightContainer}>
+        <ImageBackground
+          style={styles.backGroundImageContainer}
+          source={{ uri: imgUrl }}
+          blurRadius={2}
+          imageStyle={{ opacity: 0.8 }}
+        >
+          <Image style={styles.imageContainer} source={{ uri: imgUrl }} />
+        </ImageBackground>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  itemContainer: {
+    height: 125,
+    width: '99%',
+    borderColor: '#D6D6D6',
+    borderBottomWidth: 1,
+    flexDirection: 'row',
+    flexGrow: 0,
+  },
+  leftContainer: {
+    flex: 1,
+    padding: 10,
+    // fontSize:'20%',
+    // fontSize: RFValue(24, 580),
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+  },
+  rightContainer: {
+    margin: 10,
+    width: '40%',
+    maxWidth: 300,
+  },
+  imageContainer: {
+    flex: 1,
+    height: '95%',
+    width: '100%',
+    resizeMode: 'contain',
+  },
+  backGroundImageContainer: {
+    width: '100%',
+    height: '100%',
+  },
+  text: {
+    // fontSize: 22,
+    fontSize: RFPercentage(3),
+  },
+  title: {
+    // fontSize: 14,
+    fontSize: RFPercentage(1.5),
+    color: 'grey',
+  },
+});
