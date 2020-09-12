953c8512b269b4e78741c8b3c8882feb68fd75be
yuta hidaka <yuta322@gmail.com>
2020-09-13 02:36:59 +0900

    Add admob

--- screens/news/ArticleScreen.tsx
+++ screens/news/ArticleScreen.tsx
@@ -83,6 +83,7 @@ export default function ArticleScreen({ route, navigation }: any) {
             style={[styles.ad]}
             bannerSize="mediumRectangle"
             // adUnitID="ca-app-pub-3940256099942544/6300978111" // Test ID, Replace with your-admob-unit-id
+            adUnitID="ca-app-pub-8220669417943263~9822025915"
           />
 
           <TouchableOpacity
